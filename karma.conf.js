a28f10d35b88ec11fe40e3a3448d954633084e58
olivier.texier <olivier.texier@adventiel.fr>
2018-02-08 15:38:19 +0100

    quick

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
@@ -22,12 +23,21 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    ,
+   junitReporter: {
+     outputDir: 'coverage',
+     useBrowserName: false, // add browser name to report and classes names
+     outputFile: 'test-report.xml' // if included, results will be saved as $outputDir/$browserName/$outputFile
+   },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    chromeOptions: {
+       args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+    }
     singleRun: false
   });
 };
